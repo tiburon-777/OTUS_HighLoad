40a6a1c3f4d08be8e91ed67ec84e13ec50a93802
Andrey Ivanov <tiburon-777@users.noreply.github.com>
2021-01-26 13:37:48 +0300

    Отладка

--- pkg/dataset/generator.go
+++ pkg/dataset/generator.go
@@ -11,32 +11,33 @@ import (
 )
 
 type Person struct {
-	FirstName	string
-	SecondName	string
-	Password	string
-	BirthDate	time.Time
-	Gender		string
-	City		string
-	Interests	[]string
+	FirstName  string
+	SecondName string
+	Password   string
+	BirthDate  time.Time
+	Gender     string
+	City       string
+	Interests  []string
 }
 
 func NewPerson() (p Person) {
 	rand.Seed(time.Now().UnixNano())
-	p.Gender=genders[rand.Intn(len(genders))]
-	if p.Gender=="male" {
-		p.FirstName=manNames[rand.Intn(len(manNames))]
-		p.SecondName=secondNames[rand.Intn(len(secondNames))]
+	p.Gender = genders[rand.Intn(len(genders))]
+	if p.Gender == "male" {
+		p.FirstName = manNames[rand.Intn(len(manNames))]
+		p.SecondName = secondNames[rand.Intn(len(secondNames))]
 	} else {
-		p.FirstName=womanNames[rand.Intn(len(womanNames))]
-		p.SecondName=secondNames[rand.Intn(len(secondNames))]+"а"
+		p.FirstName = womanNames[rand.Intn(len(womanNames))]
+		p.SecondName = secondNames[rand.Intn(len(secondNames))] + "а"
 	}
-	t := make([]byte,16); rand.Read(t)
-	p.Password=string(t)
-	p.City=cities[rand.Intn(len(cities))]
-	for i:=0;i<(rand.Intn(4)+3);i++ {
-		p.Interests=append(p.Interests,interests[rand.Intn(len(interests))])
+	t := make([]byte, 16)
+	rand.Read(t)
+	p.Password = string(t)
+	p.City = cities[rand.Intn(len(cities))]
+	for i := 0; i < (rand.Intn(4) + 3); i++ {
+		p.Interests = append(p.Interests, interests[rand.Intn(len(interests))])
 	}
-	s,_ := time.ParseDuration(strconv.Itoa(rand.Intn(700000))+"h")
+	s, _ := time.ParseDuration(strconv.Itoa(rand.Intn(700000)) + "h")
 	p.BirthDate = time.Now().Add(-s)
 	return
 }
@@ -47,8 +48,8 @@ func FillDB(db *sql.DB, lim int) {
 		log.Fatalf("can't exec query: %s", err.Error())
 	}
 	log.Printf("Try to generate %d rows and fill the DB...", lim)
-	for i:=1;i<lim;i++{
-		if i%100==0 {
+	for i := 1; i < lim; i++ {
+		if i%100 == 0 {
 			log.Printf("Successfully inserted %d rows", i)
 		}
 		p := NewPerson()
@@ -62,7 +63,7 @@ func FillDB(db *sql.DB, lim int) {
 			p.City,
 			strings.Join(p.Interests, ","),
 		); err != nil {
-			log.Fatalf("can't insert row in DB. Inserted %d rows of %d", i, lim)
+			log.Fatalf("can't insert row in DB. Inserted %d rows of %d: %s", i, lim, err.Error())
 		}
 	}
 	log.Println("Table USERS filled successfully")
